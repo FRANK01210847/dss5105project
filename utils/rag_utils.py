74c47b0d81af8615d85d502dd465dbd80ba7f95d
Frank <e1597699@u.nus.edu>
2025-10-28 01:32:27 +0800

    update

--- utils/rag_utils.py
+++ utils/rag_utils.py
@@ -10,7 +10,7 @@ load_dotenv(dotenv_path=ROOT / ".env")
 
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter  # ✅ 保留
-from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
